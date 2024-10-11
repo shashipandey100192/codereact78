4729862fef26d8f5e8ddc72199b7355e249cd651
shashiweb <shashiweb64@gmail.com>
2024-10-11 19:49:57 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,24 @@
+
+const b = <h1>this is js file</h1>
+function Abc()
+{
+    // return "this is webppppppppppppppppppppp";
+    // return <h1>this is webppppppppppppppppppppp</h1>;
+    // return b;
+    return(
+        <div>
+            <h1>this  is heading</h1>
+            <h2>this is second heading</h2>
+        </div>
+    )
+    
+    
+}
+
+export default Abc;
+
+
+export function Service()
+{
+    return <h4>this is heading four</h4>
+}
---
+++ src/Contactus.ts
@@ -0,0 +1,14 @@
+function Contact()
+{
+    return "this is typescript file";
+}
+
+export default Contact
+
+
+const a:string = "this is string";
+export function Contact1()
+{
+    // return "this is second function";
+    return a
+}
