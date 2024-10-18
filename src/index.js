22ec2a374bf09ee4a5f07063d3dbe2312cb025f1
shashiweb <shashiweb64@gmail.com>
2024-10-18 20:01:33 +0530

    change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./style.css";
 import Abc from './About';
 
 import Xyz,{Contact1} from './Contactus.ts';
 import Myfunc from './Myfunctioncomp.js';
 import Myheader from './Myheader.js';
 import Myreactbootstrap from './Myreactbootstrap.js';
+import Mymui from './Mymui.js';
 
 
 
@@ -21,6 +23,8 @@ root.render(
   <Myfunc/>
   <Myheader></Myheader>
   <Myreactbootstrap/>
+  <h1>use of MUI</h1>
+  <Mymui></Mymui>
 
 
   </React.StrictMode>
